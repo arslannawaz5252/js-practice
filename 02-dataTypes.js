8cc3734cc0429b8935d6e2a734a2b889ad3fa7a4
Arslan <arslannawaz5252@gmail.com>
2025-07-29 23:05:18 +0500

    datatpes

---
+++ 02-dataTypes.js
@@ -0,0 +1,9 @@
+"use strict";   // treat all js code as new version js
+
+// alert(3+3)  //we are using nodejs ,not browser
+
+console.log(3+3)
+console.log("Arslan"+"Nawaz")
+
+console.log(typeof undefined);  //undefiend
+console.log(typeof null ); // object
