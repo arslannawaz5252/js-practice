888fe2264512c7323ba0634b93e47d329ffd3f41
Arslan <arslannawaz5252@gmail.com>
2025-07-30 00:07:44 +0500

    conversionOperation

---
+++ 03-conversionOperation.js
@@ -0,0 +1,24 @@
+let score="33abc"
+
+let isNumber=Number(score)
+console.log(isNumber)
+
+// /*33=> Number
+// "33abc"=>NaN not a number
+// */
+
+let score=0
+let Isboolean=Boolean(score)
+console.log(Isboolean)
+
+// score=" "=>false Boolean
+// score=1=> true Boolean
+// score=0=> false boolean
+// score="0"=> true Boolean
+
+
+
+
+let value="3"
+let IsString=String(value)
+console.log(typeof IsString)
