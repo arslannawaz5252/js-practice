fdaf970f1e6cc768246d303fc999df3fdddb2ed3
Arslan <arslannawaz5252@gmail.com>
2025-07-31 01:01:54 +0500

    datatype summary

--- 05-dataTypes-Summary.js
+++ 05-dataTypes-Summary.js
@@ -44,3 +44,27 @@ console.log(typeof temparture);
 
 
 
+
+
+
+
+/*stack(permitive datatype) , Heap(Non-permitive datatype)  */
+
+let myYoutubename="arsladotcom"     //stack
+let anothername=myYoutubename
+ anothername="chaiaurcode"
+
+
+console.log(myYoutubename);
+console.log(anothername);
+
+
+
+let userOne={
+    email:"arsla@.com",
+}
+
+userTwo=userOne
+ userTwo.email="ali@.com";
+console.log(userOne.email)
+console.log(userTwo.email)
