9baf1ce878a36f99c60eeb09b56dda802c4f7e61
Arslan <arslannawaz5252@gmail.com>
2025-08-09 06:46:41 +0500

    global and block scope

--- js.urdu/02-dataTypes.js
+++
@@ -1,9 +0,0 @@
-"use strict";   // treat all js code as new version js
-
-// alert(3+3)  //we are using nodejs ,not browser
-
-console.log(3+3)
-console.log("Arslan"+"Nawaz")
-
-console.log(typeof undefined);  //undefiend
-console.log(typeof null ); // object
