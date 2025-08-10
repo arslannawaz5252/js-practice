19163824fe0f09eaf47137296e7ed0ce6eaf7184
Arslan <arslannawaz5252@gmail.com>
2025-08-11 03:56:35 +0500

    imediately invokde functin express

---
+++ Function basics/04-IIFE.js
@@ -0,0 +1,20 @@
+(function chai () {
+    // name iife
+    console.log(`Db connected`);
+    
+    
+})();  // to connect two iife (;) is is used at the end of the one
+
+// that iife function is like { chai() } so there is no other myth
+
+( ()=>{
+    console.log(`Db connectd to`);
+    
+})()
+
+ // if they said to you to put parametre and execute so
+
+ ( (name)=>{
+    console.log(`Db connectd to,${name}`);
+    
+})('Arslan')
