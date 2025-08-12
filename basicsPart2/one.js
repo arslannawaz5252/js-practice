b521e6890385c6f740563c89f297e4c70c1d7a3b
Arslan <arslannawaz5252@gmail.com>
2025-08-13 02:28:02 +0500

    control flow

--- basicsPart2/one.js
+++ basicsPart2/one.js
@@ -9,7 +9,23 @@ if(true){
 console.log("Arsln logged in");
 
 
-<,>,<=,>=
+//<,>,<=,>=
 
 
+const temprature=40
+if (temprature==41) {
+    console.log("Thta's true ");
+    
+}
+else{
+    console.log("illegal information");
+    
+}
 
+
+const score=87
+//    if (score>78) console.log("tested");
+   
+
+
+    
