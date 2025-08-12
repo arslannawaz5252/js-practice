3edf4711d1312a5fb76183a9f3ba4f04a6c87824
Arslan <arslannawaz5252@gmail.com>
2025-08-13 02:47:35 +0500

    control flow

--- basicsPart2/one.js
+++ basicsPart2/one.js
@@ -26,6 +26,32 @@ else{
 const score=87
 //    if (score>78) console.log("tested");
    
-
+const balance=800
+if (balance<600) {
+    console.log("less than 600");  
+} 
+else if(balance<700) {
+    console.log("less than 700");
+    
+} 
+    
+else(balance>700) 
+    {
+        console.log("greater than 700");
+}
 
     
+const userloggedin=true
+const usrdebitCard=true
+const loggedEmail=true
+const loggedGoogle=false
+
+if (userloggedin && usrdebitCard) {  // all condition should be true
+    console.log("allowed for shopping");
+    
+}
+
+if (loggedEmail|| loggedGoogle) {   // for mulitple condition and only one should true
+    console.log("allowed for access");
+    
+}
