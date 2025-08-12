0873a469fd3c01f24196ecc852890bd9c9f2a2aa
Arslan <arslannawaz5252@gmail.com>
2025-08-13 03:52:51 +0500

    switch case

---
+++ basicsPart2/switch.js
@@ -0,0 +1,37 @@
+
+const month="march"
+
+// switch (month) {
+//     case 1:
+//         console.log("january");
+//         break;
+//     case 2:
+//         console.log("febraury");
+//         break;
+
+//         case 3:
+//         console.log("march");
+//         break;
+//     default:
+//         console.log("default case");
+        
+//         break;
+// }
+
+
+switch (month) {
+    case "january":
+        console.log("january");
+        break;
+    case "march":
+        console.log("march");
+        break;
+
+        case "aprail":
+        console.log("aprail");
+        break;
+    default:
+        console.log("default case");
+        
+        break;
+}
