ab9581cac76754975055cc80d0d31c30fe7b0107
Arslan <arslannawaz5252@gmail.com>
2025-08-14 01:00:27 +0500

    for of loop

---
+++ loops.js/forOfLoops.js
@@ -0,0 +1,50 @@
+// for of  loops
+
+const arr=[1,2,3,4]
+for (const value of arr) {
+   // console.log(value);
+    
+    
+}
+
+const greetings="Hello world"
+for (const greet of greetings) {
+    // if (greet===" ") 
+    //     {
+    //     //console.log();
+    //     //break
+    //     continue // space ignore
+    // }
+    //console.log(`Each char of ${greet}`);
+    
+}
+
+// Maps
+
+//maps are uniques object unlike objects ,they dont duplicate the value and keep the oredr same
+
+const map=new Map()
+map.set('pak',"pakistan")
+map.set('chin',"china")
+map.set('saud',"saudia Arabia")
+//map.set('pak',"pakistan")             //ignre by map due to duplication
+map.set('usa',"united state of america")
+//console.log(map);
+
+
+for (const [key,value] of map) {
+    //console.log(key ,':-',value);
+    
+}
+
+// 
+const myobject={
+    'game 1':"wcc2",
+    'game 2':"wcc3"
+}
+for (const [key,value] of myobject) {
+    console.log(key ,':-',value);
+    
+    
+}
+// so for of loop doesn't defend object itrartion
