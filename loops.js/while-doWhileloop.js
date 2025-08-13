85acabda45204199264d61be135ee03e254ad7ca
Arslan <arslannawaz5252@gmail.com>
2025-08-13 06:39:00 +0500

    while,do while

---
+++ loops.js/while-doWhileloop.js
@@ -0,0 +1,24 @@
+// 
+//    while loop
+let index=0
+while (index <=10) {
+    console.log(`value of index is ${index}`);
+    
+    index=index+3
+}
+
+
+let MyArrayA=["SPIDERMAN","ARSLAN","Ali baba"]
+let arr=0
+while (arr <MyArrayA.length) {
+    console.log(`value is ${MyArrayA[arr]}`);
+    
+    arr=arr+1
+}
+
+// do while loop
+ let score=21
+do {
+    console.log(`score is ${score}`);
+    score++
+} while (score<=12);
