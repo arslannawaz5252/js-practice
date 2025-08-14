4e076375f3c6899423ce38b02cb0b6207f5d35cf
Arslan <arslannawaz5252@gmail.com>
2025-08-15 00:30:33 +0500

    main point about loops

---
+++ loops.js/LoopsPoints.js
@@ -0,0 +1,10 @@
+// for...in → keys only (strings).
+
+// for...of → works on iterables (like arrays, Maps, or Object.entries).
+
+// That’s why you never write [key, value] in object — because for...in doesn’t give you pairs.
+
+
+----//for of loop points
+// That [key, value] syntax is array destructuring — it takes an array and
+//  unpacks its elements into separate variables___
