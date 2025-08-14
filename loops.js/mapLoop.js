b567f28a6e85b3330954d5146780b79dde84b9c6
Arslan <arslannawaz5252@gmail.com>
2025-08-15 01:59:49 +0500

    map and chain rule

---
+++ loops.js/mapLoop.js
@@ -0,0 +1,36 @@
+const mYnum=[1,2,3,4,5,6,7,8,9,10]
+// const usere=mYnum.map( (num)=>{
+//     if (num>4) {
+//        return num+2
+//     }else{
+//         return num+10
+     
+    
+//     }
+//     })
+
+// console.log(usere);
+
+
+
+const useridea=[]
+const user=mYnum.forEach( (num)=>{
+    
+        //useridea.push(num+10)
+     
+     
+}
+    
+)
+//console.log(useridea);
+
+
+// chain rule
+
+const usersak=mYnum
+              .map( (num)=> num*10)
+              .map( (num)=> num+1)
+              .filter( (num)=>num>=40)
+
+              console.log(usersak);
+              
