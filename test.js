1bb1f684af9db8ba7ac99b3c63f0e474ca8b39c8
Arslan <arslannawaz5252@gmail.com>
2025-08-25 23:42:21 +0500

    call stack

--- test.js
+++ test.js
@@ -1,1 +1,17 @@
-console.log("Arslan")
+function a() {
+     console.log('a');
+     }
+function b() {
+     a();
+      console.log('b');
+     }
+     function c () {
+         a()
+        console.log('d');
+       
+        
+     }
+b();
+a();
+c();
+
