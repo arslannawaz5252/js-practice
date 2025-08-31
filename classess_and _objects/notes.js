e1d5b57d04be8b9a02b6bc128dfcb60cfa73f79a
Arslan <arslannawaz5252@gmail.com>
2025-09-01 00:25:25 +0500

    OOP intro

---
+++ classess_and _objects/notes.js
@@ -0,0 +1,24 @@
+// javasricpt and classes
+
+//OOP
+
+// object
+--collection OF properties(variables) and methods(function)
+--toLowerCase
+
+
+// Why use oop 
+
+//Encapsulation (Data Protection)
+
+    //OOP allows hiding implementation details inside classes (using private fields #property in modern JS).
+    //OOP in JavaScript helps you model real-world entities, write cleaner code, avoid duplication, and make future changes easier — essential for large apps, games, frameworks, or APIs.
+    // Polymorphism (Same Method, Different Behavior)
+
+    // object literals
+
+    -- constructor;
+    --classs;
+    --function;
+    --prototype;
+    --instances(this,new);
